d946c6219e9705a368207c698881930676f7280c
Maurice Dure <mauricedure@gamil.com>
2021-06-15 12:46:37 -0500

    Dashboard edit

---
+++ Instructions/Starter_Code/run.sh
@@ -0,0 +1,4 @@
+panel serve dashboard.ipynb
+export MAPBOX_ACCESS_TOKEN= "pk.eyJ1IjoibWF1cmljZWR1cmUiLCJhIjoiY2trYTF0bjN5MGlrczJvbnV5dmJ5ZGIwNiJ9.AIbc8wKoI9B6Qhv-qYoXTw"
+echo $MAPBOX_ACCESS_TOKEN;
+jupyter-lab .;
